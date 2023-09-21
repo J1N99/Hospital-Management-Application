a164c485a5ab5e595520eb4ad90e62675d1a3876
J1N99 <91363943+J1N99@users.noreply.github.com>
2023-09-21 23:30:39 +0800

    After stuck on few hours, simple validation also stuck no idea but finally to fix the validation

--- app/src/main/java/com/example/hospitalmanagementapplication/userDetailsActivity.kt
+++ app/src/main/java/com/example/hospitalmanagementapplication/userDetailsActivity.kt
@@ -3,6 +3,7 @@ package com.example.hospitalmanagementapplication
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hospitalmanagementapplication.databinding.ActivityUserdetailsBinding
 import com.example.hospitalmanagementapplication.firebase.firestore
@@ -31,6 +32,7 @@ class userDetailsActivity : AppCompatActivity() {
             saveUserDetails()
         }
 
+
         binding.ageET.setOnClickListener {
 
             var ic=binding.icNumberET.text.toString()
@@ -48,6 +50,7 @@ class userDetailsActivity : AppCompatActivity() {
                     }
                     month = ic.substring(2, 4).toInt() - 1
                     day = ic.substring(4, 6).toInt()
+
 
             }
             else{
@@ -77,23 +80,63 @@ class userDetailsActivity : AppCompatActivity() {
 
     private fun saveUserDetails() {
 
-        val currentUser = firebaseAuth.currentUser
-        val userId = currentUser?.uid
-        Log.d("userId","$userId")
-        if (userId != null) {
-
-            val email=currentUser?.email?:""
-            val lastname = binding.lastNameET.text.toString()
-            val firstname = binding.firstNameEt.text.toString()
-            val dob = binding.ageET.text.toString()
-            val gender = binding.radioMale.isChecked
-            val icNumber=binding.icNumberET.text.toString()
-            val position=1
+        var lastname=binding.lastNameET.text.toString().trim()
+        var firstname=binding.firstNameEt.text.toString().trim()
+        var ic=binding.icNumberET.text.toString().trim()
+        var DOB=binding.ageET.text.toString().trim()
 
 
-            val user = User(userId,email, lastname, firstname, gender,dob,icNumber,position)
-            firestore().registerUserDetails(this,user)
 
+        var year = 0
+        var month = 0
+        var day = 0
+        var validic = true
+
+        if (lastname.isEmpty() && firstname.isEmpty() && ic.isEmpty() && DOB.isEmpty()) {
+            Toast.makeText(this, "Please enter all the fields", Toast.LENGTH_SHORT).show()
+        } else if (ic.length != 12) {
+            Toast.makeText(this, "Please enter the correct IC Number", Toast.LENGTH_SHORT).show()
+        } else {
+            year = ic.substring(0, 2).toInt()
+            month = ic.substring(2, 4).toInt() - 1
+            day = ic.substring(4, 6).toInt()
+
+            if (month < 0 || month > 11 || day < 1 || day > 31) {
+                Toast.makeText(this, "Please enter a valid IC Number", Toast.LENGTH_SHORT).show()
+                validic = false
+            } else if (DOB.isNotEmpty()) {
+                val parts = DOB.split("-")
+
+                if (year != parts[2].takeLast(2).toInt() || month != parts[1].toInt() - 1 || day != parts[0].toInt()) {
+                    Toast.makeText(this, "Please enter the correct date of birth matching the IC", Toast.LENGTH_SHORT).show()
+                    validic = false
+                }
+            }
+        }
+
+        if (validic) {
+                val currentUser = firebaseAuth.currentUser
+                val userId = currentUser?.uid
+                Log.d("userId", "$userId")
+                if (userId != null) {
+
+                    val email = currentUser?.email ?: ""
+                    val lastname = binding.lastNameET.text.toString()
+                    val firstname = binding.firstNameEt.text.toString()
+                    val dob = binding.ageET.text.toString()
+                    val gender = binding.radioMale.isChecked
+                    val icNumber = binding.icNumberET.text.toString()
+                    val position = 1
+
+
+                    val user =
+                        User(userId, email, lastname, firstname, gender, dob, icNumber, position)
+                    firestore().registerUserDetails(this, user)
+
+                }
+
+
+            }
         }
     }
-}
+
