a111306989b5a92a2bfaad511ca47153a1b0f9e6
J1N99 <91363943+J1N99@users.noreply.github.com>
2023-09-29 11:36:43 +0800

    finally it works now OMFG

--- app/src/main/java/com/example/hospitalmanagementapplication/RedesignActivity.kt
+++ app/src/main/java/com/example/hospitalmanagementapplication/RedesignActivity.kt
@@ -3,6 +3,7 @@ package com.example.hospitalmanagementapplication
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
@@ -13,16 +14,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import com.itextpdf.text.BaseColor
 import com.itextpdf.text.Document
+import com.itextpdf.text.Element
+import com.itextpdf.text.Image
 import com.itextpdf.text.PageSize
 import com.itextpdf.text.Paragraph
+import com.itextpdf.text.pdf.PdfPCell
+import com.itextpdf.text.pdf.PdfPTable
 import com.itextpdf.text.pdf.PdfWriter
 import java.io.File
 import java.io.FileOutputStream
+import com.itextpdf.text.*
+import java.io.ByteArrayOutputStream
+import java.io.IOException
 
 class RedesignActivity : AppCompatActivity() {
 
-    private val pdfFileName = "sample.pdf"
+    private val pdfFileName = "appointment_report.pdf"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,26 +53,118 @@ class RedesignActivity : AppCompatActivity() {
 
     fun createPdf(view: View) {
         if (isExternalStorageWritable()) {
-            val pdfFile =
-                File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), pdfFileName)
+            val pdfFile = File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), pdfFileName)
 
             try {
                 val document = Document(PageSize.A4)
                 val pdfWriter = PdfWriter.getInstance(document, FileOutputStream(pdfFile))
                 document.open()
 
-                // Add content to the PDF
-                val paragraph =
-                    Paragraph("Hello, this is a sample PDF created with iTextPDF in Android.")
-                document.add(paragraph)
+                // Create a table for the header
+                val headerTable = PdfPTable(2)
+                headerTable.widthPercentage = 100f
+                headerTable.setWidths(floatArrayOf(3f, 1f))
+
+                val headerCell1 = PdfPCell()
+                headerCell1.backgroundColor = BaseColor(12, 173, 128)
+                headerCell1.horizontalAlignment = Element.ALIGN_LEFT
+                headerCell1.verticalAlignment = Element.ALIGN_MIDDLE
+                headerCell1.setPadding(10f)
+
+                val headerText = Paragraph("Appointment Report", Font(Font.FontFamily.HELVETICA, 20f, Font.BOLD, BaseColor.WHITE))
+                headerText.add(Chunk.NEWLINE)
+                headerText.add("Vantist")
+                headerText.add(Chunk.NEWLINE)
+                headerText.add("No 25,Lorong Tanjung Aman 12, Taman Tanjung Aman")
+                headerText.add(Chunk.NEWLINE)
+                headerText.add("12300 Butterworth, Pulau Pinang")
+                headerText.add(Chunk.NEWLINE)
+                headerText.add(Phrase("Dr Ang Wei Jin", Font(Font.FontFamily.HELVETICA, 12f, Font.BOLD, BaseColor.WHITE)))
+                headerText.add(Chunk.NEWLINE)
+                headerText.add(Phrase("01112896803", Font(Font.FontFamily.HELVETICA, 12f, Font.BOLD, BaseColor.WHITE)))
+                headerCell1.addElement(headerText)
+
+                val headerCell2 = PdfPCell()
+                // Load the image from drawable resources
+                val imageBitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.logo)
+
+// Convert the Bitmap to a byte array
+                val byteArrayOutputStream = ByteArrayOutputStream()
+                imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+                val imageByteArray: ByteArray = byteArrayOutputStream.toByteArray()
+
+// Create an iText Image instance from the byte array
+                val image = Image.getInstance(imageByteArray)
+
+// Scale the image if needed
+                image.scaleToFit(120f, 120f)
+                headerCell2.addElement(image)
+
+                headerTable.addCell(headerCell1)
+                headerTable.addCell(headerCell2)
+
+                // Add the header table to the document
+                document.add(headerTable)
+
+                // Create a table for the content
+                val contentTable = PdfPTable(1)
+                contentTable.widthPercentage = 100f
+
+                val contentCell = PdfPCell(Paragraph("Patient Name\n" +
+                        "Chooi Chee Kean\n" +
+                        "No IC: 991125075103\n" +
+                        "Gender: Male"))
+                contentCell.setPadding(10f)
+
+                contentTable.addCell(contentCell)
+
+                // Add the content table to the document
+                document.add(contentTable)
+
+                // Create a table for the item list
+                val itemListTable = PdfPTable(4)
+                itemListTable.widthPercentage = 100f
+                itemListTable.setWidths(floatArrayOf(3f, 1f, 1f, 1f))
+
+                // Add table headers
+                itemListTable.addCell(Phrase("Item name", FontFactory.getFont(FontFactory.HELVETICA_BOLD)))
+                itemListTable.addCell(Phrase("Price", FontFactory.getFont(FontFactory.HELVETICA_BOLD)))
+                itemListTable.addCell(Phrase("Quantity", FontFactory.getFont(FontFactory.HELVETICA_BOLD)))
+                itemListTable.addCell(Phrase("Total", FontFactory.getFont(FontFactory.HELVETICA_BOLD)))
+
+                // Add item rows (You will need to loop through your data here)
+                // Example:
+                // itemListTable.addCell("Item 1")
+                // itemListTable.addCell("RM50.00")
+                // itemListTable.addCell("2")
+                // itemListTable.addCell("RM100.00")
+
+                // Add the grand total row and the thank you message
+                val grandTotalCell = PdfPCell(Phrase("Grand total: RM200.00"))
+                grandTotalCell.colspan = 4
+                itemListTable.addCell(grandTotalCell)
+
+                val thankYouCell = PdfPCell(Phrase("Thank you for your appointment.", FontFactory.getFont(FontFactory.HELVETICA_BOLD)))
+                thankYouCell.colspan = 4
+                itemListTable.addCell(thankYouCell)
+
+                val termsCell = PdfPCell(Phrase("Terms and conditions: This is an automated generated PDF; signature is not needed.", FontFactory.getFont(FontFactory.HELVETICA_BOLD)))
+                termsCell.colspan = 4
+                itemListTable.addCell(termsCell)
+
+                // Add the item list table to the document
+                document.add(itemListTable)
 
                 // Close the document
                 document.close()
 
                 Toast.makeText(this, "PDF created successfully", Toast.LENGTH_SHORT).show()
+
             } catch (e: Exception) {
                 e.printStackTrace()
+                Toast.makeText(this, "Error creating PDF", Toast.LENGTH_SHORT).show()
             }
+
         } else {
             Toast.makeText(this, "External storage not writable", Toast.LENGTH_SHORT).show()
         }
