833ab4f9833157358b2bd228a61bfaf7e4d83432
J1N99 <91363943+J1N99@users.noreply.github.com>
2023-10-16 00:33:32 +0800

    Fix adjust ui and make the dark mode

--- app/src/main/java/com/example/hospitalmanagementapplication/UpdatePositionActivity.kt
+++ app/src/main/java/com/example/hospitalmanagementapplication/UpdatePositionActivity.kt
@@ -2,9 +2,7 @@ package com.example.hospitalmanagementapplication
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hospitalmanagementapplication.databinding.ActitvityUpdatepositionBinding
 import com.example.hospitalmanagementapplication.firebase.firestore
@@ -15,13 +13,14 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
 
-class UpdatePositionActivity: AppCompatActivity() {
+class UpdatePositionActivity : AppCompatActivity() {
 
     private lateinit var binding: ActitvityUpdatepositionBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
     private lateinit var bottomNavigationView: BottomNavigationView
-    private lateinit var userId:String
+    private lateinit var userId: String
+    private lateinit var itemSelected: Any
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -33,81 +32,91 @@ class UpdatePositionActivity: AppCompatActivity() {
             if (position != null) {
                 bottomNavigationView = findViewById(R.id.bottomNavigationView)
                 bottomNavigationView.setSelectedItemId(R.id.others);
-                IntentManager(this, bottomNavigationView,position)
+                IntentManager(this, bottomNavigationView, position)
             }
         }
 
-        val spinner: Spinner = findViewById(com.example.hospitalmanagementapplication.R.id.spinner)
-        var updateID=firebaseAuth.currentUser?.uid
-        userId = intent.getStringExtra("userId") ?: ""
-        var documentID=""
-        var dbposition=0
 
-        firestore().getOtherUserDetails(this,userId) { user ->
+        var updateID = firebaseAuth.currentUser?.uid
+        userId = intent.getStringExtra("userId") ?: ""
+        var documentID = ""
+        var dbposition = 0
+
+        val items = listOf("Patient", "Doctor", "Clerk")
+        val autoComplete: AutoCompleteTextView = findViewById(R.id.autoCompleteTextView)
+        val adapter = ArrayAdapter(this, R.layout.list_private_government, items)
+        autoComplete.setAdapter(adapter)
+        autoComplete.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+
+                val position= parent.getItemAtPosition(position)
+                when (position) {
+                    "Patient" -> {
+                     itemSelected=1
+                    }
+                    "Doctor" -> {
+                        itemSelected=2
+                    }
+                    "Clerk" -> {
+                        itemSelected=3
+                    }
+
+                }
+            }
+
+        firestore().getOtherUserDetails(this, userId) { user ->
 
             if (user != null) {
-                documentID=userId
+                documentID = userId
                 Log.v("User Position", "${user.position}")
-                dbposition=user.position
-                binding.NameEt.setText(user.firstname+" "+ user.lastname)
+                dbposition = user.position
+                binding.NameEt.setText(user.firstname + " " + user.lastname)
                 binding.icnumberET.setText(user.ic)
-                binding.NameEt.isEnabled=false
-                binding.icnumberET.isEnabled=false
+                binding.NameEt.isEnabled = false
+                binding.icnumberET.isEnabled = false
                 binding.NameEt.setBackgroundResource(android.R.color.darker_gray)
                 binding.icnumberET.setBackgroundResource(android.R.color.darker_gray)
 
-                // Define an array of items to display in the Spinner
-                val positions = arrayOf("Please select position", "Patient","Doctor", "Clerk",)
+                when (user.position) {
+                    1 -> {
+                        binding.autoCompleteTextView.setText("Patient",false)
 
-                // Create an ArrayAdapter using the defined items and a default layout
-                val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, positions)
+                    }
+                    2 -> {
+                        binding.autoCompleteTextView.setText("Doctor",false)
 
-                // Specify the layout to use when the list of choices appears
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    }
+                    3 -> {
+                        binding.autoCompleteTextView.setText("Clerk",false)
 
-                // Set the ArrayAdapter to the Spinner
-                spinner.adapter = adapter
-                Log.v("Dbposition", "$dbposition")
-                // Set default selection
-                val defaultSelection = dbposition
-                Log.v("Default Selection", "$defaultSelection")
-                spinner.setSelection(defaultSelection)
+                    }
+                }
+
             } else {
                 Toast.makeText(this, "User is null", Toast.LENGTH_SHORT).show()
             }
         }
 
+        binding.button.setOnClickListener {
+            if (::itemSelected.isInitialized) {
+                val positionInt = itemSelected
 
+                val dataToUpdate = mapOf(
+                    "position" to positionInt,
+                    "updateby" to updateID as Any,
+                    "updatetime" to CurrentDateTime().getCurrentDateTimeFormatted()
+                )
 
+                firestore().updatePosition(documentID, dataToUpdate)
+            } else {
 
-
-        binding.button.setOnClickListener{
-            val selectedPosition = spinner.selectedItem.toString()
-            Log.d("Selected Position",selectedPosition)
-            var position = when (selectedPosition) {
-                "Please select position"->   "0"
-                "Patient"->"1"
-                "Doctor"->"2"
-                "Clerk" -> "3"
-                else -> "Unknown position"
-            }
-            Log.d(" Position",position)
-            var positionInt=position.toInt()
-            Log.d(" PositionInt","$positionInt")
-            if(positionInt==0)
-            {
                 Toast.makeText(
                     this,
                     "Please select position",
                     Toast.LENGTH_LONG
                 ).show()
-            }
-            else
-            {
 
-                val dataToUpdate=mapOf("position" to positionInt,"updateby" to updateID as Any,"updatetime" to CurrentDateTime().getCurrentDateTimeFormatted())
 
-                firestore().updatePosition(documentID,dataToUpdate)
             }
         }
     }
